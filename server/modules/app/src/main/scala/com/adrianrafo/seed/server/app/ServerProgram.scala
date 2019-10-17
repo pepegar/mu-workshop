33a474775c2ec1d5d82b4e125152bd0b207a296f
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-10-17 13:10:11 +0200

    step 2 - server

--- server/modules/app/src/main/scala/com/adrianrafo/seed/server/app/ServerProgram.scala
+++ server/modules/app/src/main/scala/com/adrianrafo/seed/server/app/ServerProgram.scala
@@ -17,7 +17,13 @@ class ServerProgram(implicit CE: ConcurrentEffect[IO]) extends ServerBoot {
 
     implicit val PS: PeopleService[IO] = new PeopleServiceImpl
 
-    ???
+    for {
+      peopleService <- PeopleService.bindService[IO]
+      server        <- GrpcServer.default[IO](config.port, List(AddService(peopleService)))
+      _             <- L.info(s"$serverName - Starting server at ${config.host}:${config.port}")
+      exitCode      <- GrpcServer.server(server).as(ExitCode.Success)
+    } yield exitCode
+
   }
 }
 
--- server/modules/process/src/main/scala/com/adrianrafo/seed/server/process/PeopleServiceImpl.scala
+++ server/modules/process/src/main/scala/com/adrianrafo/seed/server/process/PeopleServiceImpl.scala
@@ -17,7 +17,20 @@ class PeopleServiceImpl(implicit L: Logger[IO]) extends PeopleService[IO] {
   )
 
   def getPerson(request: PeopleRequestRPC): IO[PeopleResponseRPC] = {
-    def findPerson(request: Option[String]): Either[PeopleErrorRPC, PersonRPC] = ???
+    def findPerson(request: Option[String]): Either[PeopleErrorRPC, PersonRPC] =
+      request.fold(PersonRPC("", 0).asRight[PeopleErrorRPC])(
+        nameRequest =>
+          people.count(_.name == nameRequest) match {
+            case x if x < 2 =>
+              people
+                .find(_.name == nameRequest)
+                .fold(PeopleErrorRPC(s"Person $nameRequest not found").asLeft[PersonRPC])(
+                  _.asRight[PeopleErrorRPC]
+                )
+            case _ =>
+              PeopleErrorRPC(s"Person $nameRequest duplicated").asLeft[PersonRPC]
+          }
+      )
 
     val response = PeopleResponseRPC(findPerson(request.name))
     L.info(s"$serviceName - Request: $request - Response: $response").as(response)
